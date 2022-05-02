1d9c26597a8ce52dc2dfb321ea81d272742b2ae1
Rahaf Sami <66573092+rahafsami@users.noreply.github.com>
2022-05-02 17:48:15 +0300

    All pages are added (but not designed) except for goals page

---
+++ app/src/main/java/com/example/it342project/ArticleOne.java
@@ -0,0 +1,14 @@
+package com.example.it342project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticleOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_one);
+    }
+}
--- app/src/main/java/com/example/it342project/ArticlesMenu.java
+++ app/src/main/java/com/example/it342project/ArticlesMenu.java
@@ -3,12 +3,41 @@ package com.example.it342project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ArticlesMenu extends AppCompatActivity {
+
+    Spinner menu;
+    ArrayAdapter<CharSequence> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles_menu);
+
+        menu = (Spinner) findViewById(R.id.articlesMenuSpinner);
+
+        adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.pages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        menu.setAdapter(adapter);
+
+        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                    Toast.makeText(getBaseContext(), parent.getItemAtPosition(position).toString() + " Selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
